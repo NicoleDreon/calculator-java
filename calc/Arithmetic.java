1d859a367e2af5bdbd31605f47386e3c2e030eb1
Nicole Dreon <dreon.nicole@gmail.com>
2021-06-03 17:15:48 +0000

    complete multiply and divide methods

--- calc/Arithmetic.java
+++ calc/Arithmetic.java
@@ -10,11 +10,11 @@ public class Arithmetic {
 	}
 
 	public static float multiply(float num1, float num2) {
-		return 10.0f;
+		return num1 * num2;
 	}
 
 	public static float divide(float num1, float num2) {
-		return 10.0f;
+		return num1 / num2;
 	}
 
 	public static float square(float num1) {
